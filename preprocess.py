48d1e0a96a66029dd96e8c7f638f74fd218e9904
Anastasia <nascar.by@gmail.com>
2018-12-12 17:11:02 -0800

    Started to work on custom dataloader

--- preprocess.py
+++ preprocess.py
@@ -1,54 +1,63 @@
 import torchtext.data as data
 import torchtext.vocab as vocab
-from torchtext import data as data
-import time
+import timefrom joblib import Memory
+
 from utils import print_duration
 
+class DataProcessor():
+    def __init__(self):
+        pass
 
-def preprocess(train_csv, test_csv, tokenizer, embeddings, cache):
-    # types of csv columns
-    time_start = time.time()
-    text = data.Field(batch_first=True, tokenize=tokenizer)
-    qid = data.Field()
-    target = data.Field(sequential=False, use_vocab=False, is_target=True)
-
-    # read and tokenize data
-    print('Reading data.')
-    train = data.TabularDataset(path=train_csv, format='csv',
-                                fields={'qid': ('qid', qid),
-                                        'question_text': ('text', text),
-                                        'target': ('target', target)})
-    test = data.TabularDataset(path=test_csv, format='csv',
-                               fields={'qid': ('qid', qid),
-                                       'question_text': ('text', text)})
-    text.build_vocab(train, test, min_freq=1)
-    qid.build_vocab(train, test)
-    print_duration(time_start, 'Time to read and tokenize data: ')
-
-    # embeddings lookup
-    print('Embedding lookup...')
-    time_start = time.time()
-    text.vocab.load_vectors(vocab.Vectors(embeddings, cache=cache))
-    print_duration(time_start, 'Time for embedding lookup: ')
-
-    return train, test, text, qid
+    def read_data(train_csv, test_csv):
 
 
-def iterate(train, val, test, batch_size):
-    train_iter = data.BucketIterator(dataset=train,
-                                     batch_size=batch_size,
-                                     sort_key=lambda x: x.text.__len__(),
-                                     shuffle=True,
-                                     sort=False)
+    def preprocess(train_csv, test_csv, tokenizer, embeddings, cache):
+        # types of csv columns
+        location = './cachedir'
+        memory = Memory(location, verbose=0)
+        time_start = time.time()
+        text = data.Field(batch_first=True, tokenize=tokenizer)
+        qid = data.Field()
+        target = data.Field(sequential=False, use_vocab=False, is_target=True)
 
-    val_iter = data.BucketIterator(dataset=val,
-                                   batch_size=batch_size,
-                                   sort_key=lambda x: x.text.__len__(),
-                                   train=False,
-                                   sort=False)
+        # read and tokenize data
+        print('Reading data.')
+        train = data.TabularDataset(path=train_csv, format='csv',
+                                    fields={'qid': ('qid', qid),
+                                            'question_text': ('text', text),
+                                            'target': ('target', target)})
+        test = data.TabularDataset(path=test_csv, format='csv',
+                                   fields={'qid': ('qid', qid),
+                                           'question_text': ('text', text)})
+        print_duration(time_start, 'Time to read data?')
+        text.build_vocab(train, test, min_freq=1)
+        qid.build_vocab(train, test)
+        print_duration(time_start, 'Time to read and tokenize data: ')
 
-    test_iter = data.BucketIterator(dataset=test,
-                                    batch_size=batch_size,
-                                    sort_key=lambda x: x.text.__len__(),
-                                    sort=True)
-    return train_iter, val_iter, test_iter
+        # embeddings lookup
+        print('Embedding lookup...')
+        time_start = time.time()
+        text.vocab.load_vectors(vocab.Vectors(embeddings, cache=cache))
+        print_duration(time_start, 'Time for embedding lookup: ')
+
+        return train, test, text, qid
+
+
+    def iterate(self, train, val, test, batch_size):
+        train_iter = data.BucketIterator(dataset=train,
+                                         batch_size=batch_size,
+                                         sort_key=lambda x: x.text.__len__(),
+                                         shuffle=True,
+                                         sort=False)
+
+        val_iter = data.BucketIterator(dataset=val,
+                                       batch_size=batch_size,
+                                       sort_key=lambda x: x.text.__len__(),
+                                       train=False,
+                                       sort=False)
+
+        test_iter = data.BucketIterator(dataset=test,
+                                        batch_size=batch_size,
+                                        sort_key=lambda x: x.text.__len__(),
+                                        sort=True)
+        return train_iter, val_iter, test_iter
