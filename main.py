38230ae47f83a6f72c92c4c94ee32162a38b792c
Anastasia <nascar.by@gmail.com>
2018-12-15 21:25:01 -0800

    Refactor code in __name__ == __main__

--- main.py
+++ main.py
@@ -13,34 +13,7 @@ from utils import submit, check_changes_commited
 from create_test_datasets import reduce_embedding, reduce_datasets
 
 
-def main(args, train_csv, test_csv, embedding, cache):
-    train, test, text, qid = preprocess(train_csv, test_csv, args.tokenizer, embedding, cache)
-    random.seed(args.seed)
-    train, val = train.split(split_ratio=args.split_ratio, random_state=random.getstate())
-    train_iter, val_iter, test_iter = iterate(train, val, test, args.batch_size)
-
-    eval_every = int(len(list(iter(train_iter)))/args.n_eval)
-    if args.model == 'BiLSTM':
-        model = BiLSTM(text.vocab.vectors, lstm_layer=args.n_layers, padding_idx=text.vocab.stoi[text.pad_token], hidden_dim=args.hidden_dim, dropout=args.dropout).cuda()
-    # loss_function = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([pos_w]).cuda())
-    loss_function = nn.BCEWithLogitsLoss()
-    if args.optim == 'Adam':
-        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr)
-    elif args.optim == 'AdamW':
-        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, betas=(0.9, 0.99))
-    dataloaders = train_iter, val_iter, test_iter
-    learn = Learner(model, dataloaders, loss_function, optimizer, args)
-    learn.fit(args.epoch, eval_every, args.f1_tresh, args.early_stop, args.warmup_epoch)
-
-    # predict test labels
-    learn.load()
-    test_label, _, test_ids = learn.predict_labels(is_test=True, tresh=[0.01, 0.5, 0.01])
-    learn.record()
-    test_ids = [qid.vocab.itos[i] for i in test_ids]
-    submit(test_ids, test_label)
-
-
-if __name__ == '__main__':
+def parse_script_args():
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
 
@@ -51,12 +24,12 @@ if __name__ == '__main__':
     arg('--split_ratio', '-sr', default=0.8, type=float)
     arg('--seed', default=2018, type=int)
     arg('--tokenizer', '-t', default='spacy', choices=['spacy'])
-    arg('--embedding', '-em', default='glove', choices=['glove', 'google_news','paragram', 'wiki_news'])
+    arg('--embedding', '-em', default='glove', choices=['glove', 'google_news', 'paragram', 'wiki_news'])
 
     # training params
     arg('--optim', '-o', default='Adam', choices=['Adam', 'AdamW'])
     arg('--epoch', '-e', default=7, type=int)
-    arg('--lr','-lr', default=1e-3, type=float)
+    arg('--lr', '-lr', default=1e-3, type=float)
     arg('--batch_size', '-bs', default=512, type=int)
     arg('--n_eval', '-ne', default=1, type=int, help='Number of validation set evaluations during 1 epoch')
     arg('--warmup_epoch', '-we', default=2, type=int, help='Number of epochs without fine tuning')
@@ -64,13 +37,17 @@ if __name__ == '__main__':
     arg('--f1_tresh', '-ft', default=0.33, type=float)
 
     # model params
-    arg('--model', '-m', default = 'BiLSTM', choices=['BiLSTM'])
+    arg('--model', '-m', default='BiLSTM', choices=['BiLSTM'])
     arg('--n_layers', '-n', default=2, type=int, help='Number of layers in model')
     arg('--hidden_dim', '-hd', type=int, default=100)
     arg('--dropout', '-d', type=float, default=0.2)
 
     args = parser.parse_args()
 
+    return args
+
+
+def analyze_args(args):
     if args.embedding == 'glove':
         emb_path = 'embeddings/glove.840B.300d/glove.840B.300d.txt'
     # TODO: add other embeddings
@@ -101,4 +78,41 @@ if __name__ == '__main__':
         if not check_changes_commited():
             sys.exit("Please commit all changes!")
 
+    return train_csv, test_csv, emb_path, cache
+
+
+def main(args, train_csv, test_csv, embedding, cache):
+    train, test, text, qid = preprocess(train_csv, test_csv, args.tokenizer, embedding, cache)
+    random.seed(args.seed)
+    train, val = train.split(split_ratio=args.split_ratio, random_state=random.getstate())
+    train_iter, val_iter, test_iter = iterate(train, val, test, args.batch_size)
+
+    eval_every = int(len(list(iter(train_iter)))/args.n_eval)
+    if args.model == 'BiLSTM':
+        model = BiLSTM(text.vocab.vectors,
+                       lstm_layer=args.n_layers,
+                       padding_idx=text.vocab.stoi[text.pad_token],
+                       hidden_dim=args.hidden_dim,
+                       dropout=args.dropout).cuda()
+    # loss_function = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([pos_w]).cuda())
+    loss_function = nn.BCEWithLogitsLoss()
+    if args.optim == 'Adam':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr)
+    elif args.optim == 'AdamW':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, betas=(0.9, 0.99))
+    dataloaders = train_iter, val_iter, test_iter
+    learn = Learner(model, dataloaders, loss_function, optimizer, args)
+    learn.fit(args.epoch, eval_every, args.f1_tresh, args.early_stop, args.warmup_epoch)
+
+    # predict test labels
+    learn.load()
+    test_label, _, test_ids = learn.predict_labels(is_test=True, tresh=[0.01, 0.5, 0.01])
+    learn.record()
+    test_ids = [qid.vocab.itos[i] for i in test_ids]
+    submit(test_ids, test_label)
+
+
+if __name__ == '__main__':
+    args = parse_script_args()
+    train_csv, test_csv, emb_path, cache = analyze_args(args)
     main(args, train_csv, test_csv, emb_path, cache)
