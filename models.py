6de15c35e7971b7f13c973d30618e6da1d214bd6
Anastasia <nascar.by@gmail.com>
2018-12-21 14:51:47 -0800

    create BiLSTMPool model

--- models.py
+++ models.py
@@ -50,3 +50,28 @@ class BiGRU(nn.Module):
         gru_out, h_n = self.gru(x)
         y = self.hidden2label(self.dropout(torch.cat([h_n[i, :, :] for i in range(h_n.shape[0])], dim=1)))
         return y
+
+class BiLSTMPool(nn.Module):
+    def __init__(self, pretrained_lm, padding_idx, static=True, hidden_dim=100, lstm_layer=2, dropout=0.2):
+        super(BiLSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.dropout = nn.Dropout(p=dropout)
+        self.embedding = nn.Embedding.from_pretrained(pretrained_lm)
+        self.embedding.padding_idx = padding_idx
+        if static:
+            self.embedding.weight.requires_grad = False
+        self.lstm = nn.LSTM(input_size=self.embedding.embedding_dim,
+                            hidden_size=hidden_dim,
+                            num_layers=lstm_layer,
+                            dropout=dropout,
+                            bidirectional=True)
+        self.pool =
+        self.hidden2label = nn.Linear(hidden_dim * 6, 1)
+        self.cell = self.lstm
+
+    def forward(self, sents):
+        x = self.embedding(sents)
+        x = torch.transpose(x, dim0=1, dim1=0)
+        lstm_out, (h_n, c_n) = self.lstm(x)
+        y = self.hidden2label(self.dropout(torch.cat([h_n[i, :, :] for i in range(h_n.shape[0])], dim=1)))
+        return y
