f827de0bab11bd2dda2f49b7bf2b6a5fdcf55180
Anastasia <nascar.by@gmail.com>
2018-12-30 15:43:06 -0800

    simple custom tokenizer

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def parse_script_args():
     arg('--split_ratio', '-sr', nargs='+', default=[0.8], type=float)
     arg('--test', action='store_true') # if present split data in train-val-test else split train-val
     arg('--seed', default=2018, type=int)
-    arg('--tokenizer', '-t', default='spacy', choices=['spacy', 'whitespace'])
+    arg('--tokenizer', '-t', default='spacy', choices=['spacy', 'whitespace', 'custom'])
     arg('--embedding', '-em', default='glove', choices=['glove', 'google_news', 'paragram', 'wiki_news'])
 
     # training params
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import torchtext.data as data
 import torchtext.vocab as vocab
 import time
+import re
 
 from utils import print_duration
 from dataloader import MyTabularDataset
@@ -9,6 +10,8 @@ from dataloader import MyTabularDataset
 def choose_tokenizer(tokenizer):
     if tokenizer == 'whitespace':
         return WhitespaceTokenizer()
+    elif tokenizer == 'custom':
+        return CustomTokenizer()
     else:
         return tokenizer
 
@@ -20,6 +23,26 @@ class WhitespaceTokenizer(object):
     def __call__(self, text):
         words = text.split(' ')
         return words
+
+
+class CustomTokenizer(object):
+    def __init__(self):
+        #self.allowed_re = re.compile('^[A-Za-z0-9.,?!()]*$')
+        self.punct = re.compile('[,!.?()]')
+
+    def __call__(self, text):
+        substrings = text.split(' ')
+        tokens = []
+        for ss in substrings:
+            punct_match = self.punct.search(ss)
+            if punct_match:
+                start = punct_match.start()
+                ss_tokens = [ss[:start], ss[start], ss[start + 1:]]
+                ss_tokens = [t for t in ss_tokens if len(t) > 0]
+            else:
+                ss_tokens = [ss]
+            tokens.extend(ss_tokens)
+        return tokens
 
 
 def preprocess(train_csv, test_csv, tokenizer, embeddings, cache):
@@ -71,3 +94,8 @@ def iterate(train, val, test, batch_size):
                                         sort=False,
                                         train=False)
         return train_iter, val_iter, test_iter
+
+if __name__ == '__main__':
+    tok = CustomTokenizer()
+    str = 'I love yo-u! Yes, ,no, yes?'
+    print(tok(str))
