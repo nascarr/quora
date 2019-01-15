fe4e8c4cd5240ab762bee91d3fe9b72c367d16e8
Anastasia <nascar.by@gmail.com>
2019-01-14 17:39:03 -0800

    change default arguments at ens_main.py

--- ens_main.py
+++ ens_main.py
@@ -7,7 +7,7 @@ from ens_model_list import model_dict
 def parse_ens_main_args():
     parser = argparse.ArgumentParser(parents=[ens_parser(add_help=False)])
     arg = parser.add_argument
-    arg('--main_args', '-a', nargs='+', default=['--mode test -em glove', '--mode test -em wnews', '--mode test -em paragram'], type=str)
+    arg('--main_args', '-a', nargs='+', default=["-es 3 -e 8 -em wnews -hd 150 -we 10 --lrstep 10 -mv 700000", "-e 5 -hd 150 -mv 1500000", "-e 8 -hd 150 -em paragram -us 0 -mv 1200000"], type=str)
     args = parser.parse_args()
     return args
 
@@ -21,3 +21,5 @@ if __name__ == '__main__':
         record_dirs.append(record_dir)
     ens = Ensemble(record_dirs, args.k, model_args='paths')
     ens(args)
+
+# '--mode test -em glove', '--mode test -em wnews', '--mode test -em paragram'
