60c326e7243fe38c9fdda9ead60c0b38a1d59ee6
Anastasia <nascar.by@gmail.com>
2019-01-14 22:32:08 -0800

    50% emb cut, glove-wnews-paragram ens

--- ens_main.py
+++ ens_main.py
@@ -7,7 +7,7 @@ from ens_model_list import model_dict
 def parse_ens_main_args():
     parser = argparse.ArgumentParser(parents=[ens_parser(add_help=False)])
     arg = parser.add_argument
-    arg('--main_args', '-a', nargs='+', default=["-em glove -t whitespace"], type=str)
+    arg('--main_args', '-a', nargs='+', default=["-es 3 -e 8 -em wnews -hd 150 -we 10 --lrstep 10 -mv 500000", "-e 5 -hd 150 -mv 1100000", "-e 8 -hd 150 -em paragram -us 0 -mv 850000"], type=str)
     args = parser.parse_args()
     return args
 
