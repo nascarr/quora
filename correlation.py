5a2b8c5514243ba67fc7ca34ce7c5b1ca6750ca1
Anastasia <nascar.by@gmail.com>
2019-01-19 18:32:50 -0800

    Revert "Revert "add gnews_phrase_num tokenizer""
    This reverts commit e456136d4c63dbc8ef9302b8b6d35f0e75ca8025.

--- correlation.py
+++ correlation.py
@@ -20,4 +20,6 @@ def corr(first_model, second_model):
   print("Kendall's correlation score: {}".format(first_df[prediction].corr(second_df[prediction],method='kendall')))
   print("Spearman's correlation score: {}".format(first_df[prediction].corr(second_df[prediction],method='spearman')))
 
-corr(first_file, second_file)
+if __name__ == '__main__':
+
+  corr(first_file, second_file)
