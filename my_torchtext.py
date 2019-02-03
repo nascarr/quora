c16f95cdab8eb815073d0cc11142fac8334a4770
Anastasia <nascar.by@gmail.com>
2019-02-03 14:18:27 -0800

    cache examples

--- my_torchtext.py
+++ my_torchtext.py
@@ -6,10 +6,81 @@ import numpy as np
 import itertools
 from utils import print_duration
 import time
+import pickle
 
 
 class MyTabularDataset(TabularDataset):
     """Subclass of torch.data.dataset.TabularDataset for k-fold cross-validation"""
+    def __init__(self, path, format, fields, skip_header=False,
+                 csv_reader_params={}, **kwargs):
+        """Create a TabularDataset given a path, file format, and field list.
+
+        Arguments:
+            path (str): Path to the data file.
+            format (str): The format of the data file. One of "CSV", "TSV", or
+                "JSON" (case-insensitive).
+            fields (list(tuple(str, Field)) or dict[str: tuple(str, Field)]:
+                If using a list, the format must be CSV or TSV, and the values of the list
+                should be tuples of (name, field).
+                The fields should be in the same order as the columns in the CSV or TSV
+                file, while tuples of (name, None) represent columns that will be ignored.
+
+                If using a dict, the keys should be a subset of the JSON keys or CSV/TSV
+                columns, and the values should be tuples of (name, field).
+                Keys not present in the input dictionary are ignored.
+                This allows the user to rename columns from their JSON/CSV/TSV key names
+                and also enables selecting a subset of columns to load.
+            skip_header (bool): Whether to skip the first line of the input file.
+            csv_reader_params(dict): Parameters to pass to the csv reader.
+                Only relevant when format is csv or tsv.
+                See
+                https://docs.python.org/3/library/csv.html#csv.reader
+                for more details.
+        """
+
+        cache_path = os.path.join('.', (os.path.basename(path) + '.td'))
+        try:
+            with open(cache_path, 'rb') as f:
+                examples = pickle.load(f)
+        except:
+            format = format.lower()
+            make_example = {
+                'json': Example.fromJSON, 'dict': Example.fromdict,
+                'tsv': Example.fromCSV, 'csv': Example.fromCSV}[format]
+
+            with io.open(os.path.expanduser(path), encoding="utf8") as f:
+                if format == 'csv':
+                    reader = unicode_csv_reader(f, **csv_reader_params)
+                elif format == 'tsv':
+                    reader = unicode_csv_reader(f, delimiter='\t', **csv_reader_params)
+                else:
+                    reader = f
+
+                if format in ['csv', 'tsv'] and isinstance(fields, dict):
+                    if skip_header:
+                        raise ValueError('When using a dict to specify fields with a {} file,'
+                                         'skip_header must be False and'
+                                         'the file must have a header.'.format(format))
+                    header = next(reader)
+                    field_to_index = {f: header.index(f) for f in fields.keys()}
+                    make_example = partial(make_example, field_to_index=field_to_index)
+
+                if skip_header:
+                    next(reader)
+
+                examples = [make_example(line, fields) for line in reader]
+                with open(cache_path, 'wb') as f:
+                    pickle.dump(examples, f)
+
+        if isinstance(fields, dict):
+            fields, field_dict = [], fields
+            for field in field_dict.values():
+                if isinstance(field, list):
+                    fields.extend(field)
+                else:
+                    fields.append(field)
+
+        super(TabularDataset, self).__init__(examples, fields, **kwargs)
 
     def split(self, split_ratio=0.8, stratified=False, strata_field='target',
               random_state=None):
